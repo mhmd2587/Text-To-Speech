2a959bc9e56584ec51b6d60baf7b6a15f1d2a5d8
Mhmd.prog <118080807+mhmd2587@users.noreply.github.com>
2025-10-25 15:19:13 +0200

    Add files via upload

---
+++ Text To Speech App.js
@@ -0,0 +1,72 @@
+const textarea = document.querySelector("textarea"),
+voiceList = document.querySelector("select"),
+speechBtn = document.querySelector("button");
+
+let synth = speechSynthesis,
+isSpeaking = true;
+
+voices();
+
+function voices()
+{
+  for(let voice of synth.getVoices())   
+  {
+    let selected = voice.name ==="Google US English" ? "seleced" : 
+    "";
+    let option = `<option value="${voice.name}" ${selected}>${voice.
+    name}(${voice.lang})</option>`; 
+    voiceList.insertAdjacentHTML("beforeend", option);
+  }
+}
+
+synth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text)
+{
+    let utterance = new SpeechSynthesisUtterance(text);
+    for(let voice of synth.getVoices())
+    {
+      if(voice.name === voiceList.value)
+      {
+        utterance.voice = voice;
+      }
+    }
+   synth.speak(utterance);
+}
+
+speechBtn.addEventListener("click", e =>{
+    e.preventDefault();
+    if(textarea.value !== "")
+    {//Checks if not speaking, Speak Textarea Text
+       if(!synth.speaking)
+       {
+        textToSpeech(textarea.value);
+       } 
+       // If text was long, Add Resume and Pause Function
+       if(textarea.value.length > 80)
+       {
+        setInterval(() =>
+        {
+          if(!synth.speaking && !isSpeaking)
+          {
+           isSpeaking = true;
+           speechBtn.innerText = "Convert To speech";
+          }else{}
+        }, 500);
+        if(isSpeaking)
+        {
+         synth.resume();
+         isSpeaking = false;  
+         speechBtn.innerText = "pause Speech";
+        }else
+        {
+          synth.pause();  
+          isSpeaking = true;
+          speechBtn.innerText = "Resume Speech";
+        }
+       }else
+       {
+         speechBtn.innerText = "convert To Speech";
+       }
+    }
+});
